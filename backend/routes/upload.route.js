ffe384e3a83f5562f066fb41a3e5f47c9cddaab1
sharmarachit941@gmail.com <rachitsharma@Rachits-MacBook-Air.local>
2025-10-07 15:25:15 +0530

    add controller for content

--- backend/routes/upload.route.js
+++ backend/routes/upload.route.js
@@ -1,10 +1,12 @@
 import {Router} from 'express';
 import { verifyUserJWT } from '../middlewares/jwt.middleware.js';
 import { upload } from '../middlewares/multer.middleware.js';
-import { uploadAndCreateContent } from '../controllers/content.controller.js';
+import { uploadAndCreateContent,fetchContentById,fetchAllContents,fetchContentsByUser } from '../controllers/content.controller.js';
 
 const uploadRoutes = Router();
 
 uploadRoutes.post('/upload', verifyUserJWT, upload.single('file'), uploadAndCreateContent);
-
+uploadRoutes.get('/get-content' ,  fetchAllContents);
+uploadRoutes.get('/get-content/:id' ,  fetchContentById);
+uploadRoutes.get('/get-content-user' ,verifyUserJWT,  fetchContentsByUser);
 export default uploadRoutes;
